9ac090c4a9d4b914068a31e4b5bd049d43c9c197
EdithHarrison <36203185+EdithHarrison@users.noreply.github.com>
2024-09-17 09:31:09 -0400

    edited text

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -6,9 +6,9 @@ const Connect = () => {
       <h2 className="numbered-heading overline">What's Next?</h2>
       <h2 className="title">Get In Touch</h2>
       <p>
-      While I’m actively seeking new web development opportunities, my inbox is always open for collaboration or just a friendly hello. 
-      Whether you have a question, a project idea, or want to connect, I’ll do my best to respond. 
-      I’m excited to work with like-minded professionals and teams to create impactful digital experiences.
+      I’m actively exploring new web development opportunities, and my inbox is always open for collaboration or even just a friendly hello. 
+      Whether you have a question, a project idea, or simply want to connect, feel free to reach out — I’ll do my best to respond promptly. 
+      I’m eager to collaborate with like-minded professionals and teams to create meaningful and impactful digital experiences.
       </p>
       <a className="email-link" href="mailto:your.email@example.com">Say Hello</a>
     </section>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,9 +5,9 @@ const Intro = () => {
     <section id="home" className="intro-section">
       <h1>Hi, my name is</h1>
       <h2 className="big-heading">Edith Harrison.</h2>
-      <h3 className="big-heading">Full Stack Developer</h3>
+      <h3 className="big-heading">I'm a Full Stack Developer</h3>
       <p>
-      Building interactive websites and user-friendly digital experiences for the web.
+      Passionate about crafting interactive websites and delivering seamless, user-friendly digital experiences for the web.
       </p>
       <a href="#projects" className="button">Check out my projects!</a>
     </section>
